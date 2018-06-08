7b255b7c28fc642cef5b2a526ed485d817182d9c
HemingWu <jianmo_o@163.com>
2018-06-08 23:55:07 +0800

    文章编辑页面

--- src/views/article/writeArticle.vue
+++ src/views/article/writeArticle.vue
@@ -2,14 +2,73 @@
   <div class="write-article">
     <textarea v-model="content" class="write-area"></textarea>
     <vue-showdown :markdown="content" class="show-content"/>
-    <div class="operate"></div>
+
+    <el-button class="settings" type="info" @click="dialogFormVisible = true" icon="el-icon-setting" circle></el-button>
+    <el-dialog class="setting-content" :visible.sync="dialogFormVisible" width="50%">
+      <el-form label-position='right' label-width="80px">
+        <el-form-item label="标题">
+          <el-input ></el-input>
+        </el-form-item>
+        <el-form-item label="创建时间">
+          <el-col :span="11">
+            <el-form-item prop="date1">
+              <el-date-picker type="date" placeholder="选择日期" style="width: 100%;"></el-date-picker>
+            </el-form-item>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-form-item prop="date2">
+              <el-time-picker type="fixed-time" placeholder="选择时间" style="width: 100%;"></el-time-picker>
+            </el-form-item>
+          </el-col>
+        </el-form-item>
+        <el-form-item label="摘要">
+          <el-input type="textarea" rows="4"></el-input>
+        </el-form-item>
+        <el-form-item label="封面">
+          <el-upload
+            action="https://jsonplaceholder.typicode.com/posts/"
+            list-type="picture-card"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove">
+            <i class="el-icon-plus"></i>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
+        </el-form-item>
+        <el-form-item label="标签">
+          <el-tag
+            :key="tag"
+            v-for="tag in dynamicTags"
+            closable
+            :disable-transitions="false"
+            @close="handleClose(tag)">
+            {{tag}}
+          </el-tag>
+          <el-input
+            class="input-new-tag"
+            v-if="inputVisible"
+            v-model="inputValue"
+            ref="saveTagInput"
+            size="small"
+            @keyup.enter.native="handleInputConfirm"
+            @blur="handleInputConfirm"
+          >
+          </el-input>
+          <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogFormVisible = false">保存</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import VueShowdown from 'vue-showdown'
-
 export default {
   name: 'dashboard',
   components: {
@@ -17,7 +76,41 @@ export default {
   },
   data() {
     return {
-      content: '请写点什么'
+      content: '请写点什么',
+      dialogFormVisible: false,
+      dialogImageUrl: '',
+      dialogVisible: false,
+      dynamicTags: ['标签一', '标签二', '标签三'],
+      inputVisible: false,
+      inputValue: ''
+    }
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    },
+    handleClose(tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+    },
+
+    showInput() {
+      this.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+
+    handleInputConfirm() {
+      const inputValue = this.inputValue
+      if (inputValue) {
+        this.dynamicTags.push(inputValue)
+      }
+      this.inputVisible = false
+      this.inputValue = ''
     }
   },
   computed: {
@@ -52,12 +145,34 @@ export default {
     word-wrap:break-word;
     padding: 0 10px 0 10px;
   }
-  .operate {
-    background-color: #3a8ee6;
-    height: 100%;
-    width: 23%;
-    position: absolute;
-    right: 0;
+
+  .settings {
+    position: fixed;
+    right: 15px;
+    bottom: 15px;
   }
+  .setting-content {
+    .summary {
+      height: 100px;
+
+    }
+
+    .el-tag + .el-tag {
+      margin-left: 10px;
+    }
+    .button-new-tag {
+      margin-left: 10px;
+      height: 32px;
+      line-height: 30px;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+    .input-new-tag {
+      width: 90px;
+      margin-left: 10px;
+      vertical-align: bottom;
+    }
+  }
+
 }
 </style>
